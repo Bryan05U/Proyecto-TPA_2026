0b6f94243bc7af1e3c8f64c175fd2680c5645ba9
Joaquin Macias <omermacias@gmail.com>
2026-04-27 00:41:11 -0400

    Intentando incluir botones

--- domotica-app/src/Home.tsx
+++ domotica-app/src/Home.tsx
@@ -1,17 +1,48 @@
 import { useState } from "react";
-import Boton from "./componentes/boton";
+import Boton from "./componentes/Boton";
 import "./home.css";
 
 function Home() {
-  const [luz, setLuz] = useState<boolean>(false);
+  const [seguridad, setSeguridad] = useState(false);
+  const [escenas, setEscenas] = useState(false);
+  const [historial, setHistorial] = useState(false);
 
   return (
-    <div className="home">
-      <Boton
-        nombre="Luz sala"
-        estado={luz}
-        onClick={() => setLuz(!luz)}
-      />
+    <div className="layout">
+
+      {/* IZQUIERDA */}
+      <div className="columna">
+        <Boton
+          nombre="Seguridad"
+          estado={seguridad}
+          onClick={() => setSeguridad(!seguridad)}
+        />
+        <Boton
+          nombre="Escenas"
+          estado={escenas}
+          onClick={() => setEscenas(!escenas)}
+        />
+      </div>
+
+      {/* CENTRO */}
+      <div className="centro">
+        <img src="/logo.png" className="logo" />
+      </div>
+
+      {/* DERECHA */}
+      <div className="columna">
+        <Boton
+          nombre="Escenas"
+          estado={escenas}
+          onClick={() => setEscenas(!escenas)}
+        />
+        <Boton
+          nombre="Historial"
+          estado={historial}
+          onClick={() => setHistorial(!historial)}
+        />
+      </div>
+
     </div>
   );
 }
