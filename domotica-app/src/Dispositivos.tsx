19fef6b3c6abbe9dd4e9aecfa03d0b0f9d8933aa
Joaquin Macias <omermacias@gmail.com>
2026-04-27 03:00:29 -0400

    Agregando pantallas temporales

---
+++ domotica-app/src/Dispositivos.tsx
@@ -0,0 +1,5 @@
+function Dispositivos() {
+  return <h1>Pantalla de Dispositivos</h1>;
+}
+
+export default Dispositivos;
---
+++ domotica-app/src/Escenas.tsx
@@ -0,0 +1,5 @@
+function Escenas() {
+  return <h1>Pantalla de Escenas</h1>;
+}
+
+export default Escenas;
