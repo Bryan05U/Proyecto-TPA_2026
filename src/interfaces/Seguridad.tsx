f1d3badc3688aab3c9494bfa9b101916cc57d64b
Joaquin Macias <omermacias@gmail.com>
2026-05-03 19:21:33 -0400

    Ajustando disposición de la pantalla seguridad

--- src/interfaces/Seguridad.tsx
+++ src/interfaces/Seguridad.tsx
@@ -1,37 +1,44 @@
 import { useNavigate } from "react-router-dom";
 import Boton from "../components/Boton";
 import "../styles/Layout.css"
+import Header from "../components/Header";
 
 function Seguridad() {
   const navigate = useNavigate();
 
   return (
-    <div className="layout layout-seguridad">
+    <div className="layout">
 
-      <div className="columna columna-seguridad">
-        <Boton
-          nombre="Cámaras"
-          onClick={() => navigate("/camaras")}
-          classNameExtra="boton boton-seguridad"
-        />
-        <Boton 
-          nombre="Ventanas"
-          onClick={() => navigate("/ventanas")}
-          classNameExtra="boton boton-seguridad"
-        />
-      </div>
+      <Header titulo="SEGURIDAD" />
 
-      <div className="columna columna-seguridad">
-        <Boton 
-          nombre="Puertas"
-          onClick={() => navigate("/puertas")}
-          classNameExtra="boton boton-seguridad"
-        />
-        <Boton 
-          nombre="Temperatura"
-          onClick={() => navigate("/temperatura")}
-          classNameExtra="boton boton-seguridad"
-        />
+      <div className="contenedor layout-seguridad" >
+
+        <div className="columna columna-seguridad">
+          <Boton
+            nombre="Cámaras"
+            onClick={() => navigate("/camaras")}
+            classNameExtra="boton boton-seguridad"
+          />
+          <Boton 
+            nombre="Ventanas"
+            onClick={() => navigate("/ventanas")}
+            classNameExtra="boton boton-seguridad"
+          />
+        </div>
+
+        <div className="columna columna-seguridad">
+          <Boton 
+            nombre="Puertas"
+            onClick={() => navigate("/puertas")}
+            classNameExtra="boton boton-seguridad"
+          />
+          <Boton 
+            nombre="Temperatura"
+            onClick={() => navigate("/temperatura")}
+            classNameExtra="boton boton-seguridad"
+          />
+        </div>
+
       </div>
 
     </div>
