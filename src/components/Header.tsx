10d6ab76ec654bc21b8d375df9da6230f9a0e17f
Joaquin Macias <omermacias@gmail.com>
2026-05-25 14:06:00 -0400

    Agregando logo para volver

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,49 @@
-import "../styles/Header.css"
+import { useNavigate } from "react-router-dom";
 
-type HeaderProps = {
+import "../styles/Header.css";
+
+// ICONO TEMPORAL
+import IconoVolver from
+"../assets/Botones/Logo_Volver.svg?react";
+
+type Props = {
+
   titulo: string;
+
+  mostrarVolver?: boolean;
 };
 
-function Header({ titulo }: HeaderProps) {
+function Header({
+  titulo,
+  mostrarVolver = true
+}: Props) {
+
+  const navigate = useNavigate();
+
   return (
-    <div className="header">
+
+    <header className="header">
+
+      {/* BOTÓN VOLVER */}
+
+      {mostrarVolver && (
+
+        <button
+          className="boton-volver"
+          onClick={() => navigate(-1)}
+        >
+
+          <IconoVolver />
+
+        </button>
+
+      )}
+
+      {/* TITULO */}
+
       <h1>{titulo}</h1>
-    </div>
+
+    </header>
   );
 }
 
