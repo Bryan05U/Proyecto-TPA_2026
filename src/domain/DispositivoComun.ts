727c04c5bad0cd17d2dadf6c4c82a1f3a73a7d9f
Joaquin Macias <omermacias@gmail.com>
2026-06-06 23:10:43 -0400

    Heredando Dispositivos a DispositivosComun

--- src/domain/DispositivoComun.ts
+++ src/domain/DispositivoComun.ts
@@ -1,13 +1,13 @@
-import { Dispositivo } from "./Dispositivo";
+import { Dispositivo }
+from "./Dispositivo";
 
-export class DispositivoComun extends Dispositivo {
+export class DispositivoComun
+extends Dispositivo {
 
-  constructor(nombre: string, tipo: string) {
+  constructor(
+    nombre: string,
+    tipo: string
+  ) {
     super(nombre, tipo);
   }
-
-  ajustarIntensidad(valor: number) {
-    console.log(valor);
-  }
-
 }
