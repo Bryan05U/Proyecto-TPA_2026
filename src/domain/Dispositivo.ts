61d6f9e8add9ce9b3e7d587a84db08356f25ebbb
Joaquin Macias <omermacias@gmail.com>
2026-06-07 00:51:43 -0400

    Quitando observer momentaneamente de Dispositivo.ts

--- src/domain/Dispositivo.ts
+++ src/domain/Dispositivo.ts
@@ -1,18 +1,10 @@
-import { Observer } from "./Observer";
-
-import { Subject } from "./Subject";
-
-export class Dispositivo
-implements Subject {
+export class Dispositivo {
 
   nombre: string;
 
   tipo: string;
 
   activo: boolean;
-
-  private observers:
-    Observer[] = [];
 
   constructor(
     nombre: string,
@@ -24,41 +16,10 @@ implements Subject {
     this.activo = false;
   }
 
-  agregarObserver(
-    observer: Observer
-  ): void {
-
-    this.observers.push(
-      observer
-    );
-  }
-
-  removerObserver(
-    observer: Observer
-  ): void {
-
-    this.observers =
-      this.observers.filter(
-        o => o !== observer
-      );
-  }
-
-  notificar(): void {
-
-    this.observers.forEach(
-      observer =>
-        observer.actualizar(
-          this
-        )
-    );
-  }
-
   toggle(): void {
 
     this.activo =
       !this.activo;
-
-    this.notificar();
   }
 
   cambiarNombre(
