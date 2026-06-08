9e0eaef54fb85321f7a1d3371d41ec313b9340e6
Ianx360 <ianandres.fack@alumnos.ulagos.cl>
2026-06-07 23:30:44 -0400

    Creando casos de valiaciones

--- Login.tsx
+++ Login.tsx
@@ -14,3 +14,33 @@ const [errorPassword, setErrorPassword] = useState(false);
 
 const [isLoading, setIsLoading] = useState(false);
 const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault(); 
+  setErrorEmail(false); // Resetea el error de correo
+  setErrorPassword(false); // Resetea el error de la contraseña
+
+  // Caso 1 Si el correo no es valido
+  if (email !== USER_VALIDO) {
+    setErrorEmail(true); // Lanza la alerta de correo
+    return; 
+  }
+
+  // Caso 2, Correo valido, pero contraseña incorrecta
+  if (password !== PASS_VALIDA) {
+    setErrorPassword(true); // Lanza alerta de contraseña incorrecta
+    return; 
+  }
+
+  // Caso 3, todo correcto
+  setIsLoading(true); // Salta una pantalla de carga
+  
+  // Temporizador * Configurado a priori a 5 segundos (5000 milisegundos)
+  setTimeout(() => {
+    setIsLoading(false); 
+    setIsLoggedIn(true); 
+  }, 5000);
+};
+
+
+
