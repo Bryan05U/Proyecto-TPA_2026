7ad899b277b888e80c798c3c11d3eb3001ad8ba8
Bryan05U <168034497+Bryan05U@users.noreply.github.com>
2026-06-13 20:28:38 -0400

    Subiendo archivo ConfiguracionMenu.tsx lo que sería el componente de Configuracion.ts, extraído también de la rama desarrollo-configuracion para hacer commit de forma más segura
    Al igual que Configuracion.ts, comprobar si está bien hecho o sugerir correcciones.

---
+++ src/components/ConfiguracionMenu.tsx
@@ -0,0 +1,11 @@
+export function ConfiguracionMenu() {
+    return (
+        <>
+            <button>Cuenta</button>
+            <button>Habilitar ubicación</button>
+            <button>Compartir permisos</button>
+            <button>Modo oscuro</button>
+            <button>Cerrar sesión</button>
+        </>
+    );
+}
